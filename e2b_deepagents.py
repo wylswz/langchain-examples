170901a97bba0b4e01d48ced377d82d7c3c88da3
yunlu.wen <yunlu.wen@dify.ai>
2025-12-30 20:12:45 +0800

    e2b init

---
+++ e2b_deepagents.py
@@ -0,0 +1,27 @@
+import os
+from e2b_code_interpreter import Sandbox, CommandResult, Template
+
+
+E2B_API_KEY = os.getenv("E2B_API_KEY")
+if not E2B_API_KEY:
+    raise Exception("E2B_API_KEY is not set")
+
+
+def handle_result(result: CommandResult):
+    if result.exit_code != 0:
+        raise Exception(result.stderr)
+    print(result.stdout)
+
+def prepare_sandbox():
+    return Sandbox.create(template='runner')
+
+if __name__ == "__main__":
+    sandbox = None
+    try:
+        sandbox = prepare_sandbox()
+        sandbox.commands.run("python --version")
+        sandbox.commands.run("pip --version")
+        sandbox.commands.run("apt list")
+    finally:
+        sandbox.kill() if sandbox else None
+    
---
+++ e2b_template.py
@@ -0,0 +1,34 @@
+from e2b_code_interpreter import Template
+
+apt_dependencies = [
+    "git", "curl"
+]
+
+pip_requirements = [
+    "requests",
+    "pandas",
+    "numpy",
+    "matplotlib",
+    "seaborn",
+    "scikit-learn",
+    "scipy",
+    "markdown",
+    "pillow",
+]
+
+def prepare_template():
+    """
+    This will build a template. It's just like image building, which is time consuming.
+    make sure do this ahead of time.
+    """
+    template = Template().from_base_image().apt_install(
+        apt_dependencies
+    ).pip_install(
+        pip_requirements
+    )
+    template =Template.build(template, alias='runner', cpu_count=1, memory_mb=128)
+    return template
+
+if __name__ == "__main__":
+    template = prepare_template()
+    print(template)
